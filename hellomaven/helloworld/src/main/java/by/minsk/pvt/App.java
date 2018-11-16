72735c3a925f781af557e3b614412fe0039de487
Alex Veramkovich <veramkovich@yahoo.com>
2018-11-16 09:57:40 +0300

    Hello world for maven

---
+++ hellomaven/helloworld/src/main/java/by/minsk/pvt/App.java
@@ -0,0 +1,13 @@
+package by.minsk.pvt;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
