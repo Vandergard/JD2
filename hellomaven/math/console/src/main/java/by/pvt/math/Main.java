552d886ae570e1205ab00a306b683066c4f1e92a
Vandergard <vadim.terentev.26s@gmail.com>
2018-11-24 23:06:09 +0300

    Firts part of task "Logic"

--- hellomaven/math/console/src/main/java/by/pvt/math/Main.java
+++ hellomaven/math/console/src/main/java/by/pvt/math/Main.java
@@ -17,10 +17,14 @@ public class Main {
                 .findFirst()
                 .orElseThrow(() -> new RuntimeException("Service not found"));
 
-        int[] inputData = InputDataGenerator.generate(10, -100, 100);
-
+        //int[] inputData = InputDataGenerator.generate(10, -100, 100);
+        int[] inputData = {1,2,3,4,3,7,8,9,0,2,2,3};
         System.out.println(Arrays.toString(inputData));
 
         System.out.println(averageService.getMean(inputData));
+        for(Integer i:averageService.getMode(inputData)){
+            System.out.print(i+" ");
+        }
+        System.out.println(averageService.getMedian(inputData));
     }
 }
--- hellomaven/math/service/src/main/java/by/pvt/service/AverageServiceImpl.java
+++ hellomaven/math/service/src/main/java/by/pvt/service/AverageServiceImpl.java
@@ -2,6 +2,8 @@ package by.pvt.service;
 
 import java.security.InvalidParameterException;
 import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.HashSet;
 
 /**
  *
@@ -18,5 +20,57 @@ public class AverageServiceImpl implements AverageService {
                 .average()
                 .orElse(Double.NaN);
 
+    }
+        @Override
+    public Integer[] getMode(int[] inputData){
+        int[] countArr = new int[inputData.length];
+
+        int max=1;
+        HashSet<Integer> outputList = new HashSet<Integer>();
+
+        if (inputData == null)
+            throw new InvalidParameterException("Input data cannot be null");
+
+
+        for(int i=0; i<inputData.length; i++){
+            int element = inputData[i];
+            for(int j=0; j<inputData.length; j++){
+                if(element==inputData[j]){
+                    countArr[i]++;
+                }
+            }
+        }
+
+        for(int i=0; i<countArr.length; i++){
+            if(countArr[i]>max){
+                max = countArr[i];
+            }
+
+            if(countArr[i] != 1 && countArr[i]==max){
+                outputList.add(inputData[i]);
+            }
+        }
+        Integer[] out = outputList.toArray(new Integer[outputList.size()]);
+        return out;
+    }
+
+    @Override
+    public double getMedian(int[] inputData){
+
+        double median=0;
+        if (inputData == null)
+            throw new InvalidParameterException("Input data cannot be null");
+
+        Arrays.sort(inputData);
+
+        if (inputData.length % 2 != 0){
+            median = inputData[inputData.length/2];
+        }
+        else {
+            int m = inputData.length/2;
+            median = (inputData[m-1]+inputData[m])/2;
+        }
+
+    return median;
     }
 }
